c22556e11ce799752335e9b2990047f6079bcd92
journo10 <akifbgtr@gmail.com>
2022-10-08 03:59:35 +0300

    data ve images kısımları yapıldı.

---
+++ src/data/data.js
@@ -0,0 +1,98 @@
+import image_1 from "../images/png/senem-gumussoy4.3368e8c9.png";
+import image_2 from "../images/png/nazli-ersen.c647c455.png";
+import image_3 from "../images/png/arda-öztürk.png";
+import image_4 from "../images/png/alıkemal.39902ca4.png";
+import image_5 from "../images/png/onur-el.e31ddc4a.png";
+import image_6 from "../images/png/iremçetinJPG.0c7ab51f.png";
+import image_7 from "../images/png/mentor_samet_atdag.6c47f650.png";
+import image_8 from "../images/png/bariscan-gungor.7c5a546c.png";
+import image_9 from "../images/png/barkin-ozdemir.8eaf3e54.png";
+import image_10 from "../images/png/osman-aytaman.0322c967.png";
+import image_11 from "../images/png/yigit_erdogan.9a2557f6.png";
+import image_12 from "../images/png/kemal-hakimoglu.a1f0b4c7.png";
+
+export const data = [{
+        id: 1,
+        name: "Senem Gümüşsoy",
+        title: "Consultant @SOS Business Consultancy",
+        desc: "Senem Gümüşsoy, Dokuz Eylül Üniversitesi İktisat bölümünü bitirdi. Üniversite eğitiminden sonra bankacılık sektöründe çalıştı, sonrasında eğitim sebebiyle İngiltere’ye yerleşti. Hala İngiltere’de yaşamına devam etmekte olup, şu anda kendi danışmanlık firmasıyla girişimlere finans alanında yardımcı olmaktadır.",
+        image: image_1,
+    },
+    {
+        id: 2,
+        name: "Nazlı Erşen",
+        title: "Founder @Blink Minds",
+        desc: "Nazlı Erşen, 2006'da Columbia Üniversitesi'nde Finans bölümünden mezun olduktan sonra NYU'da İK Yönetimi, Organizasyonel Gelişim üzerine yüksek lisansını tamamladı.'Ben de varım' diyen, gelecek nesillerin hayallerine yol göstermek için 'Blink Minds'ı, 2020'nin mayıs ayında kurdu. Blink Minds gelecek nesillere güçlü, büyüyen şirketler sunmayı ve gelecek nesillerin eğitimine destek vererek öz farkındalığı yüksek bireyler yetiştirmeyi amaçlıyor.",
+        image: image_2,
+    },
+    {
+        id: 3,
+        name: "Arda Öztürk",
+        title: "Learning Experience Manager @Userspots Akademi",
+        desc: "Userspots Akademi'de Learning Experience Manager olarak görev almakta olan Arda Öztürk; bireysel ve kurumsal eğitimler, video eğitimler vermektedir. Bunların yanı sıra JR UX Designer, UX Camp ve UX Okulu projelerini yönetmektedir. Arda Öztürk; bu eğitimler sırasında ileride meslektaşı olacak gençlere yol gösterebildiği ve kariyer planlamalarında yol ışığı olabildiği için yaptığı işi çok sevdiğini vurguluyor.",
+        image: image_3,
+    },
+    {
+        id: 4,
+        name: "Ali Kemal Kapıcıoğlu",
+        title: "Trade Marketing and Sales Director @Yıldız Holding",
+        desc: "Ali Kemal Kapıcıoğlu, 2002 yılında Marmara Üniversitesi Siyaset Bilimi ve Kamu Yönetimi bölümünden mezun oldu. 2004 yılında Ülker Yıldız Holding'te işe başladı ve Yıldız Holding çatısı altında Satış, Ticari pazarlama, Proje Müdürlüğü gibi farklı alanlarda çalıştı. 2018 yılından beri Yıldız Holding'in iştiraki olan Besler Yağ-Margarin şirketinde ‘’Ticari pazarlama ve satış direktörü’’ olarak görev yapmaya devam ediyor.",
+        image: image_4,
+    },
+    {
+        id: 5,
+        name: "Onur El",
+        title: "Trade Director",
+        desc: "Onur El, çalışma hayatına radyo programcısı olarak başladı. Ardından da Agesa’da sigorta satışı sonrası Migros, Şok, Real, Kaya Holding ve Avansas’ta mağaza müdürlüğünden, ticaret direktörlüğüne tüm kademelerde görev aldı. Şimdilerde ise danışmanlık ve İTÜ Çekirdek bünyesindeki girişimlere mentörlük yapıyor.",
+        image: image_5,
+    },
+    {
+        id: 6,
+        name: "İrem Çetin",
+        title: "JRUX Designer Manager and Community - Event Manager @Userspots",
+        desc: "Userspots'da JRUX Designer Projesi’ni yönetmekte olan İrem Çetin; aynı zamanda Community&Event Manager olarak da görev almaya devam ediyor. İrem Çetin ''JRUX Designer'' programıyla birlikte gelecekteki meslektaşlarının sorunlarını çözerek onları mentorluk edebildiği için mutlu olduğunu dile getiriyor.",
+        image: image_6,
+    },
+    {
+        id: 7,
+        name: "Samet Atdağ",
+        title: "Co-Founder & CTO @Prisync",
+        desc: "İTÜ Bilgisayar Mühendisliği lisans programını tamamladıktan sonra Galatasaray Üniversitesi'nde yüksek lisans yapan Samet Atdağ, şimdilerde Boğaziçi Üniversitesi'nde Karmaşık Sistemler alanında doktorasına devam ediyor. Ayrıca Prisync’in kurucu ortağı ve CTO’su olarak görev alıyor.",
+        image: image_7,
+    },
+    {
+        id: 8,
+        name: "Barışcan Güngör",
+        title: "Co-Founder & CTO @Originium Teknoloji",
+        desc: "Barışcan Güngör, SaaS low-code ve Blockchain üzerine farklı girişimler kurmuş ve yönetmiştir. Originium Teknoloji‘de Kurucu Ortak olarak çalışmaktadır. İTÜ Çekirdek, Telekom Pilot ve benzeri oluşumlarda Teknoloji Girişimleri Mentoru ve jüri üyesi olarak ekosisteme katkı sağlamaktadır. Yıldız Teknik Üniversitesi ve Cardiff Üniversitesi Matematik Mühendisliği ve Bilgisayar Bilimleri bölümlerinden mezundur. Halen Boğaziçi Üniversitesi’nde Yapay Zeka ve Blockchain alanlarında akademik çalışmalarına devam etmekte ve MIS departmaninda yazılım derslerini vermektedir.",
+        image: image_8,
+    },
+    {
+        id: 9,
+        name: "Barkın Özdemir",
+        title: "Marketing and Programing Strategist @HyperSpace / Influencer",
+        desc: "Barkın Özdemir, İçerik yaratıcısı, küresel maceracı ve konuşmacı. Barkın, Antarktika’ya ayak basarak bütün kıtaları ziyaret eden en genç Türk oldu. Yakın zamanda da Antarktika'ya yaptığı yolculuğa dayanan bir kitap yayınladı. Kendine has hikâye anlatma tarzı ile Y ve Z kuşağına seyahat etmeleri ve bu dünyanın sunduğu her şeyi keşfetmeleri için ilham vermeyi amaçlıyor. Şu anda dijital medya ve hikâye anlatımı konularında danışmanlık yapıyor ve sıradaki macerasını kovalıyor.",
+        image: image_9,
+    },
+    {
+        id: 10,
+        name: "Osman Aytaman",
+        title: "Founder & Managing Director @Impulso Advisors",
+        desc: "Osman Aytaman, Politecnico di Milano'da Endüstri Mühendisliği bölümünden mezun oldu. Mezun olduktan sonra kariyerine İtalya'nın en büyük danışmanlık firması Value Partners'da genç danışman olarak başladı. Ardından küresel bir danışmanlık firması olan Accenture'da ve dünyanın en büyük holdinglerinden biri olan General Electric'te çalıştı. Kariyeri boyunca yönetim danışmanlığı konusundaki hayallerini gerçekleştirmek için yenilikçi, enerjik ve uygulanabilir yaklaşımlar sunmayı hedeflediği Impulso Advisors'ı kısa süre önce kurdu.",
+        image: image_10,
+    },
+    {
+        id: 11,
+        name: "Yiğit Erdoğan",
+        title: "Senior CRO Specialist @IKEA",
+        desc: "Yiğit Erdoğan, Ege Üniversitesi İstatistik mezunu ve AIESEC Alumnisi’dir. İzmir, Atina, Florida ve İstanbul'dan sonra şuan Malmö'de yaşamına devam ediyor. Son 5 senedir dijital veri analitiği ve dönüşüm optimizasyonu üzerine çalışan Yiğit Erdoğan, Yemeksepeti’nin ardından IKEA’da Senior CRO Specialist olarak kariyer yolculuğuna devam etmektedir.",
+        image: image_11,
+    },
+    {
+        id: 12,
+        name: "Kemal Hakimoğlu",
+        title: "Italy Technology Sales Manager @Randstad",
+        desc: "Kemal Hakimoğlu, Dokuz Eylül Üniversitesi Çalışma Ekonomisi ve Endüstri İlişkileri bölümünden mezun olmuştur. 10 yılı aşkın kariyerinde teknoloji ve insan kaynakları alanında deneyim kazanmıştır. SaaS alanında farklı satış görevlerinde bulunmuştur. İTÜ Çekirdek, Yıldız Teknopark ve AIESEC gibi kurumlarda teknoloji girişimleri mentoru olarak destek vermektedir. Hali hazırda Randstad İtalya Teknoloji Satış Müdürü olarak görevine devam etmektedir.",
+        image: image_12,
+    },
+]

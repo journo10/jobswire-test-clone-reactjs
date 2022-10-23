701cd1b7bdc7aae45a05ab5d39ea6caf334e0ff6
journo10 <akifbgtr@gmail.com>
2022-10-23 16:45:44 +0300

    footer comp bitti.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,20 +1,22 @@
 import React from "react";
-import "./Navbar.css"
+import "./Navbar.css";
 
 const Footer = () => {
   return (
     <footer>
       <div className="footer">
         <h2>JobSwire App çok yakında sizlerle, beklemede kalın!</h2>
-        <div className="footer-text">
-          <span>Copyright © 2021 JobSwire All Rights Reserved</span>
-          <span>Gizlilik Politikası ve Aydınlatma Metni </span>
-          <span>Çerez Politikası</span>
-        </div>
-        <div className="footer-icons">
-          <i class="fa-brands fa-github"></i>
-          <i class="fa-brands fa-linkedin"></i>
-          <i class="fa-brands fa-twitter"></i>
+        <div className="footer-bottom">
+          <div className="footer-text">
+            <span>Copyright © 2021 JobSwire All Rights Reserved</span>
+            <span>Gizlilik Politikası ve Aydınlatma Metni </span>
+            <span>Çerez Politikası</span>
+          </div>
+          <div className="footer-icons">
+            <i className="fa-brands fa-github"></i>
+            <i className="fa-brands fa-linkedin"></i>
+            <i className="fa-brands fa-twitter"></i>
+          </div>
         </div>
       </div>
     </footer>

0d1cd2d7fb16a36a11dd5fd714f1c314839c40a1
journo10 <akifbgtr@gmail.com>
2022-11-02 15:45:24 +0300

    modal comp ...

--- src/pages/MentorCard/index.js
+++ src/pages/MentorCard/index.js
@@ -35,9 +35,6 @@ const MentorCard = ({ item }) => {
           <h5 className="m-name">{item.name}</h5>
           <h6 className="m-title">{item.title}</h6>
           <p  className="m-desc">{item.desc}</p>
-          <button type="button" className="m-btn" onClick={onClose}>
-            <i className="fa-solid fa-xmark"></i>
-          </button>
         </div>
       </Modal>
     </div>
--- src/pages/Modal/index.js
+++ src/pages/Modal/index.js
@@ -1,23 +1,17 @@
 import React from "react";
 import "./Modal.css";
 
-const Modal = ({ openModal, onClose, item, children}) => {
+const Modal = ({ openModal, onClose, item, children }) => {
   if (!openModal) {
     return null;
   }
 
   return (
-    <div className="m-card" >
+    <div className="m-card">
       <div className="modal">
-        {/* <img src={item.image} alt="m-images" />
-        <div className="modal-text">
-          <h5 className="m-name">{item.name}</h5>
-          <h6 className="m-title">{item.title}</h6>
-          <p  className="m-desc">{item.desc}</p>
-          <button type="button" className="m-btn" onClick={onClose}>
-            <i className="fa-solid fa-xmark"></i>
-          </button>
-        </div> */}
+        <button type="button" className="m-btn" onClick={onClose}>
+          <i className="fa-solid fa-xmark"></i>
+        </button>
         {children}
       </div>
     </div>

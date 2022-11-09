2801bac4cb7b3eddbb2d65656a01b1e7af399a52
journo10 <akifbgtr@gmail.com>
2022-11-09 21:38:28 +0300

    form gönderme işlemi

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,35 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Contact.css";
 import contact_img from "../../images/svg/contact-img.e6f4167f.svg";
 
 const Contact = () => {
+  const [form, setForm] = useState({
+    username: "",
+    email: "",
+    subject: "",
+    textarea: "",
+  });
+
+  //onChange
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  //onSubmit
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    alert(JSON.stringify(form))
+    console.log(JSON.stringify(form));
+  };
+
   return (
     <div>
       <h1>İletişim</h1>
-      <p className="c-title">Aklına takılan tüm sorularda bize ulaşabilirsin!</p>
+      <p className="c-title">
+        Aklına takılan tüm sorularda bize ulaşabilirsin!
+      </p>
       <div className="container-form">
         <img src={contact_img} alt="form" />
-        <form>
+        <form onSubmit={onSubmitForm}>
           <input
+            value={form.username}
+            onChange={handleChange}
+            name="username"
             className="form-input"
             type="text"
             placeholder="Adınız Soyadınız"
           />
           <input
+            value={form.email}
+            onChange={handleChange}
+            name="email"
             className="form-input"
             type="text"
             placeholder="E-mail Adresiniz "
           />
-          <input className="form-input" type="text" placeholder="Konu" />
+          <input
+            value={form.subject}
+            onChange={handleChange}
+            name="subject"
+            className="form-input"
+            type="text"
+            placeholder="Konu"
+          />
           <textarea
-            className="form-textarea"
+            value={form.textarea}
+            onChange={handleChange}
             name="textarea"
+            className="form-textarea"
             id="textarea-form"
             cols="30"
             rows="10"
             placeholder="Mesajınız"
           ></textarea>
-          <button type="submit" className="form-btn">
+          <button
+            type="submit"
+            className="form-btn"
+            disabled={
+              !form.username || !form.email || !form.subject || !form.textarea
+            }
+          >
             Gönder
           </button>
         </form>
--- src/pages/HiringButtonModal/index.js
+++ src/pages/HiringButtonModal/index.js
@@ -1,34 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import "./HiringButtonModal.css";
 
 const HiringButtonModal = ({ itemsHiringData, setOpenModal }) => {
+  const [form, setForm] = useState({
+    username: "",
+    lastname: "",
+    email: "",
+    file: "",
+  });
+
+  //close
   const closeModal = () => {
     setOpenModal(false);
+  };
+
+  //onChange
+  const handleChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  //onSubmit
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    console.log(JSON.stringify(form));
   };
 
   return (
     <div>
       <h2>CV Gönder</h2>
       <p>{itemsHiringData.title}</p>
-      <form>
+      <form onSubmit={onSubmitForm}>
         <div className="f-input">
           <label>İsminiz</label>
-          <input type="text" />
+          <input
+            value={form.username}
+            onChange={handleChange}
+            name="username"
+            type="text"
+          />
         </div>
         <div className="f-input">
           <label>Soyisminiz</label>
-          <input type="text" />
+          <input
+            value={form.lastname}
+            onChange={handleChange}
+            name="lastname"
+            type="text"
+          />
         </div>
         <div className="f-input">
           <label>E-mailiniz</label>
-          <input type="email" />
+          <input
+            value={form.email}
+            onChange={handleChange}
+            name="email"
+            type="email"
+          />
         </div>
         <div className="f-file">
           <label htmlFor="upload-input">Dosya Seç</label>
-          <input id="upload-input" className="f-file-input" type="file" />
+          <input
+            value={form.file}
+            onChange={handleChange}
+            name="file"
+            id="upload-input"
+            className="f-file-input"
+            type="file"
+          />
         </div>
         <div className="form-button">
-          <button className="f-btn" type="submit">
+          <button
+            disabled={
+              !form.username || !form.lastname || !form.email || !form.file
+            }
+            className="f-btn"
+            type="submit"
+          >
             Gönder
           </button>
           <button className="f-btn" type="button" onClick={closeModal}>
